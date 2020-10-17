68d9a2f8a0a7b9bf85de2faf26ffe4a5437e17ce
Zhijiang Li <zl256@cornell.edu>
2020-10-17 00:59:49 -0700

    Add photos with lfs, modify gallery photos

--- photo-website/src/photos.ts
+++ photo-website/src/photos.ts
@@ -1,110 +1,64 @@
+const app_uri = `${window.location.hostname}${window.location.port ? ':'+window.location.port: ''}/photos`;
+
 export const photos = [
   {
-    src: "https://source.unsplash.com/2ShvY8Lf6l0/1600x1200",
+    // horizontal
+    src: `${app_uri}/ZLIS3199-sun-provia.jpg`,
     srcSet: [
-      "https://source.unsplash.com/2ShvY8Lf6l0/500x375 500w",
-      "https://source.unsplash.com/2ShvY8Lf6l0/800x600 800w",
-      "https://source.unsplash.com/2ShvY8Lf6l0/1024x768 1024w",
-      "https://source.unsplash.com/2ShvY8Lf6l0/1600x1200 1600w"
+      `${app_uri}/ZLIS3199-sun-provia.jpg?nf_resize=fit&w=1080&h=720 1080w`
     ],
     sizes: ["(min-width: 480px) 50vw,(min-width: 1024px) 33.3vw,100vw"],
-    width: 4,
-    height: 3
+    width: 3,
+    height: 2
   },
   {
-    src: "https://source.unsplash.com/Dm-qxdynoEc/1600x1600",
+    // horizontal
+    src: `${app_uri}/ZLIS3146-frames.jpg`,
     srcSet: [
-      "https://source.unsplash.com/Dm-qxdynoEc/500x500 500w",
-      "https://source.unsplash.com/Dm-qxdynoEc/800x800 800w",
-      "https://source.unsplash.com/Dm-qxdynoEc/1024x1024 1024w",
-      "https://source.unsplash.com/Dm-qxdynoEc/1600x1600 1600w"
+      `${app_uri}/ZLIS3146-frames.jpg?nf_resize=fit&w=1080&h=720 1080w`
     ],
     sizes: ["(min-width: 480px) 50vw,(min-width: 1024px) 33.3vw,100vw"],
-    width: 6,
-    height: 6
+    width: 3,
+    height: 2
   },
   {
-    src: "https://source.unsplash.com/qDkso9nvCg0/1200x1600",
+    // horizontal
+    src: `${app_uri}/ZLIS3142-dutchlight.jpg`,
     srcSet: [
-      "https://source.unsplash.com/qDkso9nvCg0/375x500 375w",
-      "https://source.unsplash.com/qDkso9nvCg0/600x800 600w",
-      "https://source.unsplash.com/qDkso9nvCg0/768x1024 768w",
-      "https://source.unsplash.com/qDkso9nvCg0/1200x1600 1200w"
+      `${app_uri}/ZLIS3142-dutchlight.jpg?nf_resize=fit&w=1080&h=720 1080w`
     ],
     sizes: ["(min-width: 480px) 50vw,(min-width: 1024px) 33.3vw,100vw"],
-    width: 6,
-    height: 8
+    width: 3,
+    height: 2
   },
   {
-    src: "https://source.unsplash.com/iecJiKe_RNg/1200x1600",
+    // horizontal
+    src: `${app_uri}/ZLIS3199-sun-provia.jpg`,
     srcSet: [
-      "https://source.unsplash.com/iecJiKe_RNg/375x500 375w",
-      "https://source.unsplash.com/iecJiKe_RNg/600x800 600w",
-      "https://source.unsplash.com/iecJiKe_RNg/768x1024 768w",
-      "https://source.unsplash.com/iecJiKe_RNg/1200x1600 1200w"
+      `${app_uri}/ZLIS3199-sun-provia.jpg?nf_resize=fit&w=1080&h=720 1080w`
     ],
     sizes: ["(min-width: 480px) 50vw,(min-width: 1024px) 33.3vw,100vw"],
-    width: 6,
-    height: 8
+    width: 3,
+    height: 2
   },
   {
-    src: "https://source.unsplash.com/epcsn8Ed8kY/1200x1600",
+    // horizontal
+    src: `${app_uri}/ZLIS3199-sun-provia.jpg`,
     srcSet: [
-      "https://source.unsplash.com/epcsn8Ed8kY/375x500 375w",
-      "https://source.unsplash.com/epcsn8Ed8kY/600x800 600w",
-      "https://source.unsplash.com/epcsn8Ed8kY/768x1024 768w",
-      "https://source.unsplash.com/epcsn8Ed8kY/1200x1600 1200w"
+      `${app_uri}/ZLIS3199-sun-provia.jpg?nf_resize=fit&w=1080&h=720 1080w`
     ],
     sizes: ["(min-width: 480px) 50vw,(min-width: 1024px) 33.3vw,100vw"],
-    width: 6,
-    height: 8
+    width: 3,
+    height: 2
   },
   {
-    src: "https://source.unsplash.com/NQSWvyVRIJk/1600x1200",
+    // horizontal
+    src: `${app_uri}/ZLIS3199-sun-provia.jpg`,
     srcSet: [
-      "https://source.unsplash.com/NQSWvyVRIJk/500x375 500w",
-      "https://source.unsplash.com/NQSWvyVRIJk/800x600 800w",
-      "https://source.unsplash.com/NQSWvyVRIJk/1024x768 1024w",
-      "https://source.unsplash.com/NQSWvyVRIJk/1600x1200 1600w"
+      `${app_uri}/ZLIS3199-sun-provia.jpg?nf_resize=fit&w=1080&h=720 1080w`
     ],
     sizes: ["(min-width: 480px) 50vw,(min-width: 1024px) 33.3vw,100vw"],
-    width: 8,
-    height: 6
-  },
-  {
-    src: "https://source.unsplash.com/zh7GEuORbUw/1200x1600",
-    srcSet: [
-      "https://source.unsplash.com/zh7GEuORbUw/375x500 375w",
-      "https://source.unsplash.com/zh7GEuORbUw/600x800 600w",
-      "https://source.unsplash.com/zh7GEuORbUw/768x1024 768w",
-      "https://source.unsplash.com/zh7GEuORbUw/1200x1600 1200w"
-    ],
-    sizes: ["(min-width: 480px) 50vw,(min-width: 1024px) 33.3vw,100vw"],
-    width: 6,
-    height: 6
-  },
-  {
-    src: "https://source.unsplash.com/PpOHJezOalU/1600x1200",
-    srcSet: [
-      "https://source.unsplash.com/PpOHJezOalU/500x375 500w",
-      "https://source.unsplash.com/PpOHJezOalU/800x600 800w",
-      "https://source.unsplash.com/PpOHJezOalU/1024x768 1024w",
-      "https://source.unsplash.com/PpOHJezOalU/1600x1200 1600w"
-    ],
-    sizes: ["(min-width: 480px) 50vw,(min-width: 1024px) 33.3vw,100vw"],
-    width: 8,
-    height: 6
-  },
-  {
-    src: "https://source.unsplash.com/I1ASdgphUH4/1600x1200",
-    srcSet: [
-      "https://source.unsplash.com/I1ASdgphUH4/500x375 500w",
-      "https://source.unsplash.com/I1ASdgphUH4/800x600 800w",
-      "https://source.unsplash.com/I1ASdgphUH4/1024x768 1024w",
-      "https://source.unsplash.com/I1ASdgphUH4/1600x1200 1600w"
-    ],
-    sizes: ["(min-width: 480px) 50vw,(min-width: 1024px) 33.3vw,100vw"],
-    width: 8,
-    height: 6
+    width: 3,
+    height: 2
   }
 ];
