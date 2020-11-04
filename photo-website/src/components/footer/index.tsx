530a13603111041af6aa50a0f2e2ba1d10edc4fc
Zhijiang Li <zl256@cornell.edu>
2020-11-03 23:23:46 -0800

    Clean footer import

--- photo-website/src/components/footer/index.tsx
+++ photo-website/src/components/footer/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import style from './style.module.scss';
-import { Instagram, Copyright } from '@material-ui/icons';
+import { Copyright } from '@material-ui/icons';
 
 
 function Footer() {
