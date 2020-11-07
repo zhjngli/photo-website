d3fed91c5d8ef01173e909b530e3c22e245777d7
Zhijiang Li <zl256@cornell.edu>
2020-11-06 23:29:40 -0800

    Refactor theming and fix spacing across all components

--- photo-website/src/components/content/contact.tsx
+++ photo-website/src/components/content/contact.tsx
@@ -7,7 +7,7 @@ class Contact extends React.Component {
       <div className={style.container}>
         <p className={style.text}>
           Let's create something!
-          </p>
+        </p>
         <p className={style.text}>
           Message me on <a href="https://www.instagram.com/zhjngli/" target="_blank" rel="noreferrer noopener" className={style.link}>Instagram</a>.
         </p>
--- photo-website/src/index.tsx
+++ photo-website/src/index.tsx
@@ -1,17 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import { ThemeProvider } from '@material-ui/core/styles';
-import theme from './theme';
 import * as serviceWorker from './serviceWorker';
 import Page from './page';
 
 ReactDOM.render(
-  <ThemeProvider theme={theme}>
-    {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-    <CssBaseline />
-    <Page />
-  </ThemeProvider>,
+  <Page />,
   document.querySelector('#root'),
 );
 
