d80531e9f2d6ea98f9094c185c68b8aefb5fcd19
Zhijiang Li <zl256@cornell.edu>
2020-11-07 23:15:38 -0800

    Move images to public

--- photo-website/src/photos.ts
+++ photo-website/src/photos.ts
@@ -1,11 +1,11 @@
-import clouds from './images/DSCF0299-1.jpg';
+// import clouds from './images/DSCF0299-1.jpg';
 
 export const photos = [
   {
     // horizontal
-    src: `${clouds}`,
+    src: `dev/null`,
     srcSet: [
-      `${clouds}?nf_resize=fit&w=1080 1080w`
+      `dev/null?nf_resize=fit&w=1080 1080w`
     ],
     sizes: ["(min-width: 480px) 50vw,(min-width: 1024px) 33.3vw,100vw"],
     width: 3,
