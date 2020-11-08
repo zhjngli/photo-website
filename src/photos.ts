49491afb4a47f859a37077fa209ff829e62da644
Zhijiang Li <zl256@cornell.edu>
2020-11-08 13:41:38 -0800

    Fix photo aspect ratio

--- src/photos.ts
+++ src/photos.ts
@@ -2,13 +2,13 @@ import clouds from './photos/201910-cloudbloom.jpg';
 
 export const photos = [
   {
-    // horizontal
+    // vertical
     src: `${clouds}`,
     srcSet: [
       `${clouds}?nf_resize=fit&w=1080 1080w`
     ],
     sizes: ["(min-width: 480px) 50vw,(min-width: 1024px) 33.3vw,100vw"],
-    width: 3,
-    height: 2
+    width: 2,
+    height: 3
   }
 ];
