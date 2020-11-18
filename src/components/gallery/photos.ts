a7ecff3b7498c7a4cbb8784ed8061da2e25d0cc8
Zhijiang Li <zl256@cornell.edu>
2020-11-18 00:53:27 -0800

    Curating images

--- src/components/gallery/photos.ts
+++ src/components/gallery/photos.ts
@@ -1,5 +1,5 @@
 import p01 from '../../assets/photos/201910-diagonalcloud.jpg';
-import p02 from '../../assets/photos/201910-cloudbloom.jpg';
+// import p02 from '../../assets/photos/201910-cloudbloom.jpg';
 import p03 from '../../assets/photos/201911-coffee.jpg';
 import p04 from '../../assets/photos/201911-forestrays.jpg';
 import p05 from '../../assets/photos/202003-neighborhood.jpg';
@@ -11,8 +11,8 @@ import p10 from '../../assets/photos/202009-provia.jpg';
 import p11 from '../../assets/photos/202009-zigzag.jpg';
 import p12 from '../../assets/photos/202010-alhambra.jpg';
 import p13 from '../../assets/photos/202010-atlantic.jpg';
-import p14 from '../../assets/photos/202010-cloudtexture.jpg';
-import p15 from '../../assets/photos/202010-keysview.jpg';
+// import p14 from '../../assets/photos/202010-cloudtexture.jpg';
+// import p15 from '../../assets/photos/202010-keysview.jpg';
 
 const defaultSizes = [`
   (min-width: 480px) 50vw,
@@ -21,18 +21,18 @@ const defaultSizes = [`
   `];
 
 export const photos = [
-  {
-    src: `${p15}`,
-    sizes: defaultSizes,
-    width: 2,
-    height: 3
-  },
-  {
-    src: `${p14}`,
-    sizes: defaultSizes,
-    width: 3,
-    height: 2
-  },
+  // {
+  //   src: `${p15}`,
+  //   sizes: defaultSizes,
+  //   width: 2,
+  //   height: 3
+  // },
+  // {
+  //   src: `${p14}`,
+  //   sizes: defaultSizes,
+  //   width: 3,
+  //   height: 2
+  // },
   {
     src: `${p13}`,
     sizes: defaultSizes,
@@ -99,12 +99,12 @@ export const photos = [
     width: 2,
     height: 3
   },
-  {
-    src: `${p02}`,
-    sizes: defaultSizes,
-    width: 2,
-    height: 3
-  },
+  // {
+  //   src: `${p02}`,
+  //   sizes: defaultSizes,
+  //   width: 2,
+  //   height: 3
+  // },
   {
     src: `${p01}`,
     sizes: defaultSizes,
