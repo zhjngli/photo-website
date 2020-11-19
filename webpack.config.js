38864d5be7fc8e04a98ac7644dfba243bb866172
Zhijiang Li <zl256@cornell.edu>
2020-11-19 00:52:16 -0800

    Fix baseUrl in metatags deployment

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ const TerserPlugin = require("terser-webpack-plugin");
 const isProd = process.env.NODE_ENV === 'production';
 const outputDir = 'dist';
 
-const baseUrl = 'https://zhjngli-photo.netlify.app';
+const baseUrl = 'https://metatags--zhjngli-photo.netlify.app';
 const metaTitle = "Zhijiang Li";
 const metaDescription = "Photo gallery of selected works.";
 const metaImageName = 'me.jpg';
