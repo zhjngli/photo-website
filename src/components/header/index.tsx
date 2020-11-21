5dfbf6708e06d762520e92f19a10e182e5af6caa
Zhijiang Li <zl256@cornell.edu>
2020-11-20 23:29:08 -0800

    Add Instagram link component with analytics

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import InstaLink from '../instaLink';
 import style from './style.module.scss';
 
 class Header extends React.Component {
@@ -16,9 +17,7 @@ class Header extends React.Component {
           <NavLink to="/contact" className={style.links} activeClassName={style.activeNavLink}>
             contact
           </NavLink>
-          <a href="https://www.instagram.com/zhjngli/" target="_blank" rel="noreferrer noopener" className={style.links}>
-            instagram
-          </a>
+          <InstaLink style={style.links} />
         </div>
       </div>
     );
