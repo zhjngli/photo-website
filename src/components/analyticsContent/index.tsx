a47d68859b2002cc770ed4f1becfff19074a9a1d
Zhijiang Li <zl256@cornell.edu>
2020-11-24 00:06:26 -0800

    Remove logging when gtagging

--- src/components/analyticsContent/index.tsx
+++ src/components/analyticsContent/index.tsx
@@ -4,8 +4,8 @@ import AnalyticsContentProps from "./types";
 function withAnalytics<P>(Component: React.ComponentType<P>) {
   return class extends React.Component<P & AnalyticsContentProps> {
     componentDidMount() {
-      console.log("location: ", window.location.href);
-      console.log("gtagging from: ", this.props.pageTitle, this.props.pagePath);
+      // console.log("location: ", window.location.href);
+      // console.log("gtagging from: ", this.props.pageTitle, this.props.pagePath);
       window.gtag('config', 'G-R0M2056RBS', {
         'page_title' : this.props.pageTitle,
         'page_path': this.props.pageTitle
