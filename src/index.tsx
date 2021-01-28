a86b7f7051f4862d8d29c00349580cd4dd76c675
Zhijiang Li <zl256@cornell.edu>
2021-01-28 01:22:03 -0800

    Revert "Register service worker"
    This reverts commit de3b4a411390548bce18280605b7bd4d49484237.

--- src/index.tsx
+++ src/index.tsx
@@ -23,4 +23,4 @@ ReactDOM.render(<Page />, document.querySelector('#root'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-serviceWorker.register();
+serviceWorker.unregister();
