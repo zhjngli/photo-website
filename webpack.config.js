473199f9ef13cd5812120182ee019cddb470e0de
Zhijiang Li <zl256@cornell.edu>
2021-01-30 12:45:03 -0800

    Add bundle analyzer for dev builds

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,7 @@ const PreloadWebpackPlugin = require('preload-webpack-plugin');
 const RobotstxtPlugin = require("robotstxt-webpack-plugin");
 const TerserPlugin = require("terser-webpack-plugin");
 const WorkboxPlugin = require('workbox-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 const isProd = process.env.NODE_ENV === 'production';
 const outputDir = 'dist';
@@ -17,146 +18,151 @@ const metaDescription = "Photo gallery of selected works.";
 const metaImageName = 'me.jpg';
 const metaImage = baseUrl + '/' + metaImageName;
 
-module.exports = {
-  plugins: [
-    new CopyPlugin({
-      // webpack won't bundle any content not being used in src. copy it instead
-      patterns: [
-        { // metatag
-          from: 'src/assets/meta/metatag-1-1,5.jpg',
-          transformPath(targetPath, absolutePath) {
-            return metaImageName;
-          },
+const plugins = [
+  new CopyPlugin({
+    // webpack won't bundle any content not being used in src. copy it instead
+    patterns: [
+      { // metatag
+        from: 'src/assets/meta/metatag-1-1,5.jpg',
+        transformPath(targetPath, absolutePath) {
+          return metaImageName;
         },
-        { // favicons
-          from: "*",
-          context: path.resolve(__dirname, "src", "assets", "favicons"),
-        },
-        { // self-hosted
-          from: "**/*",
-          context: path.resolve(__dirname, "src", "assets", "self-hosted"),
-          to: "i/[path]/[contenthash].[ext]",
+      },
+      { // favicons
+        from: "*",
+        context: path.resolve(__dirname, "src", "assets", "favicons"),
+      },
+      { // self-hosted
+        from: "**/*",
+        context: path.resolve(__dirname, "src", "assets", "self-hosted"),
+        to: "i/[path]/[contenthash].[ext]",
+      }
+    ],
+  }),
+  new MiniCssExtractPlugin({
+    filename: '[name].[hash].min.css',
+    chunkFilename: '[id].[hash].min.css'
+  }),
+  new HtmlWebpackPlugin({
+    template: 'public/index.html',
+    filename: 'index.html',
+    favicon: 'src/assets/favicons/favicon.ico',
+    meta: {
+      'viewport': 'width=device-width, initial-scale=1',
+      'description': metaDescription,
+    }
+  }),
+  new HtmlWebpackTagsPlugin({
+    metas: [
+      {
+        attributes: {
+          property: 'og:url',
+          content: baseUrl
         }
-      ],
-    }),
-    new MiniCssExtractPlugin({
-      filename: '[name].[hash].min.css',
-      chunkFilename: '[id].[hash].min.css'
-    }),
-    new HtmlWebpackPlugin({
-      template: 'public/index.html',
-      filename: 'index.html',
-      favicon: 'src/assets/favicons/favicon.ico',
-      meta: {
-        'viewport': 'width=device-width, initial-scale=1',
-        'description': metaDescription,
-      }
-    }),
-    new HtmlWebpackTagsPlugin({
-      metas: [
-        {
-          attributes: {
-            property: 'og:url',
-            content: baseUrl
+      },
+      {
+        attributes: {
+          property: 'og:type',
+          content: 'website'
+        }
+      },
+      {
+        attributes: {
+          property: 'og:title',
+          content: metaTitle
+        }
+      },
+      {
+        attributes: {
+          property: 'og:description',
+          content: metaDescription
+        }
+      },
+      {
+        attributes: {
+          property: 'og:image',
+          content: metaImage,
+        }
+      },
+      {
+        attributes: {
+          property: 'og:image:type',
+          content: "image/jpeg"
+        }
+      },
+      {
+        attributes: {
+            property: 'og:image:width',
+            content: "1080"
+        }
+      },
+      {
+        attributes: {
+            property: 'og:image:height',
+            content: "1618"
+        }
+      },
+      {
+        attributes: {
+          name: 'twitter:card',
+          content: 'summary'
+        }
+      },
+      {
+        attributes: {
+          name: 'twitter:site',
+          content: '@zhjngli'
+        }
+      },
+      {
+        attributes: {
+          name: 'twitter:title',
+          content: metaTitle,
+        }
+      },
+      {
+        attributes: {
+          name: 'twitter:description',
+          content: metaDescription
+        }
+      },
+      {
+        attributes: {
+          name: 'twitter:image',
+          content: metaImage
+        }
+      },
+    ]
+  }),
+  new PreloadWebpackPlugin({
+    rel: 'preload',
+    as: 'font',
+    include: 'allAssets',
+    fileWhitelist: [/\.(woff2?|ttf|otf)(\?.*)?$/i],
+  }),
+  new WorkboxPlugin.GenerateSW({
+    // these options encourage the ServiceWorkers to get in there fast
+    // and not allow any straggling "old" SWs to hang around
+    clientsClaim: true,
+    skipWaiting: true,
+  }),
+  new RobotstxtPlugin({
+    options: {
+      policy: [
+          {
+            userAgent: "*",
+            disallow: "",
           }
-        },
-        {
-          attributes: {
-            property: 'og:type',
-            content: 'website'
-          }
-        },
-        {
-          attributes: {
-            property: 'og:title',
-            content: metaTitle
-          }
-        },
-        {
-          attributes: {
-            property: 'og:description',
-            content: metaDescription
-          }
-        },
-        {
-          attributes: {
-            property: 'og:image',
-            content: metaImage,
-          }
-        },
-        {
-          attributes: {
-            property: 'og:image:type',
-            content: "image/jpeg"
-          }
-        },
-        {
-          attributes: {
-              property: 'og:image:width',
-              content: "1080"
-          }
-        },
-        {
-          attributes: {
-              property: 'og:image:height',
-              content: "1618"
-          }
-        },
-        {
-          attributes: {
-            name: 'twitter:card',
-            content: 'summary'
-          }
-        },
-        {
-          attributes: {
-            name: 'twitter:site',
-            content: '@zhjngli'
-          }
-        },
-        {
-          attributes: {
-            name: 'twitter:title',
-            content: metaTitle,
-          }
-        },
-        {
-          attributes: {
-            name: 'twitter:description',
-            content: metaDescription
-          }
-        },
-        {
-          attributes: {
-            name: 'twitter:image',
-            content: metaImage
-          }
-        },
       ]
-    }),
-    new PreloadWebpackPlugin({
-      rel: 'preload',
-      as: 'font',
-      include: 'allAssets',
-      fileWhitelist: [/\.(woff2?|ttf|otf)(\?.*)?$/i],
-    }),
-    new WorkboxPlugin.GenerateSW({
-      // these options encourage the ServiceWorkers to get in there fast
-      // and not allow any straggling "old" SWs to hang around
-      clientsClaim: true,
-      skipWaiting: true,
-    }),
-    new RobotstxtPlugin({
-      options: {
-        policy: [
-            {
-              userAgent: "*",
-              disallow: "",
-            }
-        ]
-      }
-    })
-  ],
+    }
+  })
+];
+if (!isProd) {
+  plugins.push(new BundleAnalyzerPlugin());
+}
+
+module.exports = {
+  plugins: plugins,
   entry: './src/index.tsx',
   module: {
     // consider: https://www.npmjs.com/package/common-config-webpack-plugin
