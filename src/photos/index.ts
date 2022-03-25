83ad9409a9c6269b6fc2c045300635a8dd4773f1
Zhijiang Li <zl256@cornell.edu>
2022-03-25 00:53:40 -0700

    Hash photos for carousel links

--- src/photos/index.ts
+++ src/photos/index.ts
@@ -1,5 +1,6 @@
 import { PhotoProps } from 'react-photo-gallery';
 
 // defaults for typecheck to succeed
-export type ExtendedPhotoProps = { webpSrc?: string };
+export type ExtendedPhotoProps = { webpSrc?: string; hash?: string };
+export const photosReverseIndex = {} as Record<string, number>;
 export default [] as PhotoProps<ExtendedPhotoProps>[];
